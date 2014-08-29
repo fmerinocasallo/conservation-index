319c9101a468b804281c32bf6f49721001b4919f
F. Merino-Casallo <fmerino@unizar.es>
2014-08-29 11:42:36 +0200

    Removed old and useless files.
    We have change the directory 'conservation-index' to
    'conservation_index', so the original one is no longer active.

--- conservation-index/ld_time_init.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3.3
-import timeit
-
-for k in range(1, 10):
-	size = 10000 * k
-	time = timeit.timeit("[{'-': 0.0, 'A': 0.0, 'G': 0.0, 'C': 0.0, 'T': 0.0}] * size",
-                             setup="from __main__ import size")
-	print('time init {:d}: {:.4f} s'.format(size, time))
-
